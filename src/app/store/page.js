ce0f16d9c1121ac2f6be33abe483b9d9372f0290
aditya-sharma02 <adyashma786@gmail.com>
2023-08-16 23:52:42 +0530

    initial commit

--- src/app/store/page.js
+++ src/app/store/page.js
@@ -1,5 +1,5 @@
 "use client"
-import { configureStore } from "@reduxjs/toolkit";
+import  configureStore  from "@reduxjs/toolkit";
 import dataSlice from "./slice/dataSlice";
 
 
--- src/app/store/provider.js
+++ src/app/store/provider.js
@@ -1,5 +1,5 @@
 "use client"
-import { Provider } from "react-redux";
+import  Provider  from "react-redux";
 import store from "./page";
 
 
